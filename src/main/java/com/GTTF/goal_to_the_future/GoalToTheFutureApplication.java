5432a6a14c69842f085c7d8cad9b5311586f13ef
seobbang <seobbangway@naver.com>
2022-11-08 18:55:26 +0900

    스프링 & 리액트 통합 개발 환경 구축

---
+++ src/main/java/com/GTTF/goal_to_the_future/GoalToTheFutureApplication.java
@@ -0,0 +1,13 @@
+package com.GTTF.goal_to_the_future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalToTheFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalToTheFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTTF/goal_to_the_future/GoalToTheFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTTF.goal_to_the_future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalToTheFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
