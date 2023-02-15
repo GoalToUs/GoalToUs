ff64f9eaecbeb090efba392eec6f9e5b5a0d904e
leedabeen <binys1121@ewhain.net>
2023-02-15 17:56:51 +0900

    마이페이지 조회

---
+++ src/main/java/com/GTTF/goal_to_the_future/domain/user/dto/response/MypageResponseDto.java
@@ -0,0 +1,19 @@
+package com.GTTF.goal_to_the_future.domain.user.dto.response;
+
+import com.GTTF.goal_to_the_future.domain.team.entity.Team;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+
+@Getter
+@AllArgsConstructor
+public class MypageResponseDto {
+    private String photo;
+    private String name;
+    private String birth;
+    private String teamName;
+    private String position;
+
+}
