ca0a9c6e42a0e9a65e02ccca10ee2642e71a006e
seobbang <seobbangway@naver.com>
2023-03-10 16:40:39 +0900

    feat/#15: ã팀 등록, 가입 관련 리 hooks

--- src/main/frontend/src/hooks/team.js
+++ src/main/frontend/src/hooks/team.js
@@ -1,9 +1,26 @@
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import {postTeamInfo} from "../apis/team";
+import {useMutation, useQuery, useQueryClient} from 'react-query';
+import {fetchSearchTeam, postTeamInfo, postJoinTeam} from "../apis/team";
 
+// 팀 등록하기
 export const usePostUserInfo = () => {
     const queryClient = useQueryClient();
     return useMutation(postTeamInfo, {
+        onSuccess() {
+            console.log("success");
+        },
+    });
+}
+
+// 팀 검색하기
+export const  useFetchSearchTeam = (searchWord) => {
+    const {data} = useQuery(["searchWord"], () => fetchSearchTeam(searchWord));
+    return data;
+}
+
+//팀 가입하기
+export const usePostJoinTeam = () => {
+    const queryClient = useQueryClient();
+    return useMutation(postJoinTeam, {
         onSuccess() {
             console.log("success");
         },
