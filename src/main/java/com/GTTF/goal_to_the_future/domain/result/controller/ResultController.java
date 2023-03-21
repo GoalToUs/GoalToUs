edc4c3148b55d5f7987da5c1711f53b2219b6c7b
leedabeen <binys1121@ewhain.net>
2023-03-21 13:44:19 +0900

    result

--- src/main/java/com/GTTF/goal_to_the_future/domain/result/controller/ResultController.java
+++ src/main/java/com/GTTF/goal_to_the_future/domain/result/controller/ResultController.java
@@ -1,2 +1,9 @@
-package com.GTTF.goal_to_the_future.domain.result.controller;public class ResultController {
+package com.GTTF.goal_to_the_future.domain.result.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ResultController {
 }
