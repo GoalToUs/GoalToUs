aef8ab4965f7e252e66afa82c22f05123dac18d3
leedabeen <binys1121@ewhain.net>
2023-05-17 13:00:23 +0900

    팀 검색 수정

--- src/main/java/com/GTTF/goal_to_the_future/domain/team/controller/TeamController.java
+++ src/main/java/com/GTTF/goal_to_the_future/domain/team/controller/TeamController.java
@@ -8,6 +8,7 @@ import com.GTTF.goal_to_the_future.domain.team.dto.response.CreateTeamResponseDt
 import com.GTTF.goal_to_the_future.domain.team.dto.response.InfoResponseDto;
 import com.GTTF.goal_to_the_future.domain.team.dto.response.JoinTeamResponseDto;
 import com.GTTF.goal_to_the_future.domain.team.dto.response.SearchTeamResponseDto;
+import com.GTTF.goal_to_the_future.domain.team.entity.Team;
 import com.GTTF.goal_to_the_future.domain.team.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -35,9 +36,15 @@ public class TeamController {
     public InfoResponseDto getTeamInfo(@RequestParam String teamName){
         return teamService.getTeamInfo(teamName);
     }
-    @GetMapping("/team/list")
-    public List<SearchTeamResponseDto> searchTeamInfo(@RequestParam(required = false) String search ,@RequestParam String teamName){
-        return teamService.searchTeamInfo(search);
+
+//    @GetMapping("/team/list")//팀 검색하기
+//    public List<SearchTeamResponseDto> searchTeamInfo(@RequestParam(required = false) String search ,@RequestParam String teamName){
+//        return teamService.searchTeamInfo(search);
+//    }
+
+    @GetMapping("/team/list/{keyword}")
+    public List<SearchTeamResponseDto> searchTeam(@PathVariable String keyword){
+        return teamService.search(keyword);
     }
 
 
