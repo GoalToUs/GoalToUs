ae7d36d14915f2ee3a551f516447489937bb4023
leedabeen <binys1121@ewhain.net>
2023-05-23 15:44:41 +0900

    모든 경기 목록 보기

---
+++ src/main/java/com/GTTF/goal_to_the_future/domain/result/dto/response/ViewRecordResponseDto.java
@@ -0,0 +1,17 @@
+package com.GTTF.goal_to_the_future.domain.result.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ViewRecordResponseDto {
+    private Integer goal;
+    private Integer penaltyKick;
+    private Integer yellowCard;
+    private Integer redCard;
+    private String heatmap;
+    private String ballHeatmap;
+    private Integer pass;
+
+}
