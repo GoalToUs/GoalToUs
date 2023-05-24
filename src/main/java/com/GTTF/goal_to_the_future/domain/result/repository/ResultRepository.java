8ae14836794adb9d1b0cc058f583f9d28b2fea44
leedabeen <binys1121@ewhain.net>
2023-05-25 01:31:53 +0900

    ì1:31

--- src/main/java/com/GTTF/goal_to_the_future/domain/result/repository/ResultRepository.java
+++ src/main/java/com/GTTF/goal_to_the_future/domain/result/repository/ResultRepository.java
@@ -1,14 +1,22 @@
 package com.GTTF.goal_to_the_future.domain.result.repository;
 
 import com.GTTF.goal_to_the_future.domain.match.entity.Match;
+import com.GTTF.goal_to_the_future.domain.result.dto.response.ViewRecordResponseDto;
 import com.GTTF.goal_to_the_future.domain.result.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result,Long> {
     @Override
     Optional<Result> findById(Long matchId);
+
+    @Query("select new com.GTTF.goal_to_the_future.domain.result.dto.response.ViewRecordResponseDto(r.goal,r.penaltyKick,r.yellowCard,r.redCard,r.heatmap,r.ballHeatmap,r.pass,r.team.id)"
+            +"from Result r join r.match m " +
+            " where m.matchId =: matchId ")
+    List<ViewRecordResponseDto> findALLRecord();
 }
