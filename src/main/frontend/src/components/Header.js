b8006b50230791728f8e49cae759290941ed5dac
seobbang <seobbangway@naver.com>
2023-05-25 15:53:03 +0900

    로그인 true 고정

--- src/main/frontend/src/components/Header.js
+++ src/main/frontend/src/components/Header.js
@@ -1,58 +1,66 @@
-import styled from 'styled-components';
-import {ProfileIcon} from "../assets";
-import {useRecoilValue} from "recoil";
-import {loginState} from "../states/user";
-import {Link} from "react-router-dom";
+import styled from "styled-components";
+import { ProfileIcon } from "../assets";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function Header({noRightSection, noLogo}) {
-    const isLogined = useRecoilValue(loginState);
-    return (
-        <Styled.Container>
-             <Styled.Logo href={"/"}><Link to={"/"}>{!noLogo && "GoalToUs"}</Link></Styled.Logo>
-            {!noRightSection && (isLogined ? <img src={ProfileIcon}/> : <Styled.loginJoinButton href={"/login"}>로그인/회원가입</Styled.loginJoinButton>)}
-        </Styled.Container>
-    );
+function Header({ noRightSection, noLogo }) {
+  const isLogined = useState(true);
+  return (
+    <Styled.Container>
+      <Styled.Logo href={"/"}>
+        <Link to={"/"}>{!noLogo && "GoalToUs"}</Link>
+      </Styled.Logo>
+      {!noRightSection &&
+        (isLogined ? (
+          <img src={ProfileIcon} />
+        ) : (
+          <Styled.loginJoinButton href={"/login"}>
+            로그인/회원가입
+          </Styled.loginJoinButton>
+        ))}
+    </Styled.Container>
+  );
 }
 
 export default Header;
 
 const Styled = {
-    Container : styled.header`
+  Container: styled.header`
     display: flex;
     align-items: center;
-    justify-content : space-between;
+    justify-content: space-between;
     padding: 0 50px;
-    
+
     height: 90px;
-    
+
     & > img {
-    width : 40px;
-    height: 40px;
-    cursor : pointer;
+      width: 40px;
+      height: 40px;
+      cursor: pointer;
     }
-    `,
-    Logo : styled.div`
+  `,
+  Logo: styled.div`
     font-size: 30px;
     font-weight: bold;
-    
-    cursor : pointer
-    `,
-    loginJoinButton : styled.a`
+
+    cursor: pointer;
+  `,
+  loginJoinButton: styled.a`
     display: flex;
     justify-content: center;
     align-items: center;
 
     background: none;
     border: none;
-    
+
     font-weight: 800;
     font-size: 14px;
     color: #494949;
-    
+
     cursor: pointer;
-    
+
     &:hover {
-        text-decoration: underline;
+      text-decoration: underline;
     }
-    `
-}
+  `,
+};
